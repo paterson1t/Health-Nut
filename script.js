b9ed6ab2784e577549e19e6f10d2cf9267be28f6
paterson1t <paterson1t@gmail.com>
2018-06-04 05:35:43 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+/*script.js*/
+$(document).ready(function() { 
+	$(".list-services .tooltips").easyTooltip();
+}); 
+
+$(window).load(function() {
+	$('#bgSlider').bgSlider({
+		duration:1000,
+		pagination:'.pagination',
+		preload:true,
+		slideshow:30000,
+		spinner:'.bg_spinner'
+	});
+});
